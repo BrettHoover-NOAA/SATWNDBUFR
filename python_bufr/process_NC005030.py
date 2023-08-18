516d397b561da54f976832918a133da73c0a5ad7
BrettHoover-NOAA <Brett.Hoover@noaa.gov>
2023-08-18 15:46:46 -0400

    Moved pre-QC checks to an internal function in python_bufr/process_NC005030.py

--- python_bufr/process_NC005030.py
+++ python_bufr/process_NC005030.py
@@ -22,6 +22,69 @@ def bufr_query(bufrFile, queryString, queryName):
         r = f.execute(q)
     return r.get(queryName)
 
+# pre_qc: perform pre-QC checks on input data, return indices of pass/fail obs
+#
+# INPUTS:
+#    pre: pressure, float(nobs,), hPa
+#    spd: wind speed, float(nobs,), m/s
+#    zen: zenith, angle float(nobs,), deg
+#    qin: quality indicator w/o forecast, int(nobs,), 0-100 index
+#    cov: coefficient of variation, float(nobs,), fractional coefficient
+#    exp: expected error, float(nobs,), m/s packed into 10. - 0.1*exp format
+#
+# OUTPUTS:
+#    idxPass: indices of observations passing all checks
+#    idxFail: indices of observations failing at least one check
+#
+# DEPENDENCIES:
+#    numpy
+def pre_qc(pre, spd, zen, qin, cov, exp):
+    import numpy as np
+    # generate vector of all indices and copy to idxPass
+    idxAll = np.arange(np.size(pre))
+    idxPass = np.copy(idxAll)
+    # zenith angle check
+    angMax = 68.
+    checkPass = np.where(zen <= angMax)
+    checkFail = np.setdiff1d(idxAll, checkPass)
+    idxPass = np.setdiff1d(idxPass, checkFail)
+    print('{:d} observations fail zenith angle check, {:d} pass'.format(np.size(checkFail), np.size(checkPass)))
+    # quality indicator check
+    qiMin = 90
+    qiMax = 100
+    checkPass = np.where((qin >= qiMin) & (qin <= qiMax))
+    checkFail = np.setdiff1d(idxAll, checkPass)
+    idxPass = np.setdiff1d(idxPass, checkFail)
+    print('{:d} observations fail quality indicator check, {:d} pass'.format(np.size(checkFail), np.size(checkPass)))
+    # pressure check
+    preMin = 15000.
+    checkPass = np.where(pre >= preMin)
+    checkFail = np.setdiff1d(idxAll, checkPass)
+    idxPass = np.setdiff1d(idxPass, checkFail)
+    print('{:d} observations fail pressure check, {:d} pass'.format(np.size(checkFail), np.size(checkPass)))
+    # coefficient of variation check
+    covMin = 0.04
+    covMax = 0.50
+    checkPass = np.where((cov >= covMin) & (cov <= covMax))
+    checkFail = np.setdiff1d(idxAll, checkPass)
+    idxPass = np.setdiff1d(idxPass, checkFail)
+    print('{:d} observations fail coefficient of variation check, {:d} pass'.format(np.size(checkFail), np.size(checkPass)))
+    # exp-errnorm check
+    expErrNorm = 100. * np.ones(np.size(exp,))
+    speedExists = np.where(spd > 0.1)
+    expErrNorm[speedExists] = np.divide(10. - 0.1*exp[speedExists], spd[speedExists])
+    eeMax = 0.9
+    checkPass = np.where(expErrNorm <= eeMax)
+    checkFail = np.setdiff1d(idxAll, checkPass)
+    idxPass = np.setdiff1d(idxPass, checkFail)
+    print('{:d} observations fail exp-errnorm check, {:d} pass'.format(np.size(checkFail), np.size(checkPass)))
+    # define idxFail as all indices not in idxPass
+    idxFail = np.setdiff1d(idxAll, idxPass)
+    print('{:d} OBSERVATIONS FAIL ALL QC, {:d} PASS'.format(np.size(idxFail), np.size(idxPass)))
+    # return
+    return idxPass, idxFail
+
+
 latitude = np.asarray([])
 longitude = np.asarray([])
 pressure = np.asarray([])
@@ -55,46 +118,12 @@ for key in list(queryDict.keys()):
         coefficientOfVariation = np.append(coefficientOfVariation, x[:,0].squeeze())
     else:
         print('unknown key: ' + key)
-idxAll = np.arange(np.size(latitude))
-idxPass = np.copy(idxAll)
 
-# zenith angle check
-angMax = 68.
-checkPass = np.where(zenithAngle <= angMax)
-checkFail = np.setdiff1d(idxAll, checkPass)
-idxPass = np.setdiff1d(idxPass, checkFail)
-print('{:d} observations fail zenith angle check, {:d} pass'.format(np.size(checkFail), np.size(checkPass)))
-# quality indicator check
-qiMin = 90
-qiMax = 100
-checkPass = np.where((qualityIndicator >= qiMin) & (qualityIndicator <= qiMax))
-checkFail = np.setdiff1d(idxAll, checkPass)
-idxPass = np.setdiff1d(idxPass, checkFail)
-print('{:d} observations fail quality indicator check, {:d} pass'.format(np.size(checkFail), np.size(checkPass)))
-# pressure check
-preMin = 15000.
-checkPass = np.where(pressure >= preMin)
-checkFail = np.setdiff1d(idxAll, checkPass)
-idxPass = np.setdiff1d(idxPass, checkFail)
-print('{:d} observations fail pressure check, {:d} pass'.format(np.size(checkFail), np.size(checkPass)))
-# coefficient of variation check
-covMin = 0.04
-covMax = 0.50
-checkPass = np.where((coefficientOfVariation >= covMin) & (coefficientOfVariation <= covMax))
-checkFail = np.setdiff1d(idxAll, checkPass)
-idxPass = np.setdiff1d(idxPass, checkFail)
-print('{:d} observations fail coefficient of variation check, {:d} pass'.format(np.size(checkFail), np.size(checkPass)))
-# exp-errnorm check
-expErrNorm = 100. * np.ones(np.size(expectedError,))
-speedExists = np.where(windSpeed > 0.1)
-expErrNorm[speedExists] = np.divide(10. - 0.1*expectedError[speedExists], windSpeed[speedExists])
-eeMax = 0.9
-checkPass = np.where(expErrNorm <= eeMax)
-checkFail = np.setdiff1d(idxAll, checkPass)
-idxPass = np.setdiff1d(idxPass, checkFail)
-print('{:d} observations fail exp-errnorm check, {:d} pass'.format(np.size(checkFail), np.size(checkPass)))
-
-
-
-idxFail = np.setdiff1d(idxAll, idxPass)
-print('{:d} OBSERVATIONS FAIL ALL QC, {:d} PASS'.format(np.size(idxFail), np.size(idxPass)))
+# perform pre-QC checks
+idxPass, idxFail = pre_qc(pre=pressure,
+                          spd=windSpeed,
+                          zen=zenithAngle,
+                          qin=qualityIndicator,
+                          cov=coefficientOfVariation,
+                          exp=expectedError)
+print(np.size(idxFail), np.size(idxPass))
