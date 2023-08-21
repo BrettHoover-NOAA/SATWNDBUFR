6a7568ba1088fdf2f2644c5efc23ae0f93a57cbb
BrettHoover-NOAA <Brett.Hoover@noaa.gov>
2023-08-21 18:14:15 -0400

    added python_bufr/modulefiles/BUFR/hera.lua to quick-load stack-intel-oneapi-mpi for using bufr.cpython

---
+++ python_bufr/modulefiles/BUFR/hera.lua
@@ -0,0 +1,31 @@
+help([[
+Load environment for running the BUFR application with python and MPI.
+]])
+
+local pkgName    = myModuleName()
+local pkgVersion = myModuleVersion()
+local pkgNameVer = myModuleFullName()
+
+conflict(pkgName)
+
+prepend_path("MODULEPATH", '/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.3.1/envs/unified-env/install/modulefiles/Core')
+prepend_path("MODULEPATH", '/scratch1/NCEPDEV/da/python/opt/modulefiles/stack')
+
+-- below two lines get us access to the spack-stack modules
+load("stack-intel/2021.5.0")
+load("stack-intel-oneapi-mpi/2021.5.1")
+
+setenv("CC","mpiicc")
+setenv("FC","mpiifort")
+setenv("CXX","mpiicpc")
+
+local mpiexec = '/apps/slurm/default/bin/srun'
+local mpinproc = '-n'
+setenv('MPIEXEC_EXEC', mpiexec)
+setenv('MPIEXEC_NPROC', mpinproc)
+
+
+whatis("Name: ".. pkgName)
+whatis("Version: ".. pkgVersion)
+whatis("Category: BUFR")
+whatis("Description: Load all libraries needed for BUFR")
