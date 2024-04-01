22adf9eeb831786384b8e01a725560182359115a
BrettHoover-NOAA <Brett.Hoover@noaa.gov>
2024-04-01 13:22:20 -0400

    update modulefiles/BUFR/hera.lua for updated Hera dependencies

--- python_bufr/modulefiles/BUFR/hera.lua
+++ python_bufr/modulefiles/BUFR/hera.lua
@@ -8,12 +8,15 @@ local pkgNameVer = myModuleFullName()
 
 conflict(pkgName)
 
-prepend_path("MODULEPATH", '/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.3.1/envs/unified-env/install/modulefiles/Core')
+prepend_path("MODULEPATH", '/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.6.0/envs/unified-env-rocky8/install/modulefiles/Core')
 prepend_path("MODULEPATH", '/scratch1/NCEPDEV/da/python/opt/modulefiles/stack')
 
 -- below two lines get us access to the spack-stack modules
 load("stack-intel/2021.5.0")
 load("stack-intel-oneapi-mpi/2021.5.1")
+-- need libeckit.so, libbufr_4.so
+load("eckit/1.24.5")
+load("bufr/12.0.1")
 
 setenv("CC","mpiicc")
 setenv("FC","mpiifort")
